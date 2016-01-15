3cc2f52d306a532c7d2fe9e47181240cf816512e
bajb <brooke@bajb.net>
2016-01-15 12:26:21 +0000

    Tmp Token Storage

--- src/OAuth/TokenStorage/TmpFileTokenStorage.php
+++ src/OAuth/TokenStorage/TmpFileTokenStorage.php
@@ -1,6 +1,7 @@
 <?php
 namespace Fortifi\Api\Core\OAuth\TokenStorage;
 
+use Fortifi\Api\Core\OAuth\Tokens\IToken;
 use Packaged\Helpers\Path;
 
 class TmpFileTokenStorage implements TokenStorageInterface
@@ -10,29 +11,44 @@ class TmpFileTokenStorage implements TokenStorageInterface
    *
    * @param string $key location key to store the token in
    *
-   * @param string $token
+   * @param IToken $token
    *
    * @return bool
    */
-  public function storeToken($key, $token)
+  public function storeToken($key, IToken $token)
   {
-    return file_put_contents($this->_createFileName($key), $token) !== false;
+    return file_put_contents(
+      $this->_createFileName($key),
+      serialize($token)
+    ) !== false;
   }
 
   /**
    * Retrieve a token from storage
    *
-   * @param string $key location key for token
+   * @param string   $key      location key for token
+   * @param callable $retrieve method to retireve token
    *
-   * @return string|null
+   * @return IToken|null
    */
-  public function retrieveToken($key)
+  public function retrieveToken($key, callable $retrieve = null)
   {
     $location = $this->_createFileName($key);
     if(file_exists($location))
     {
-      return file_get_contents($location);
+      return unserialize(file_get_contents($location));
     }
+
+    if($retrieve !== null)
+    {
+      $token = $retrieve();
+      if($token instanceof IToken)
+      {
+        $this->storeToken($key, $token);
+        return $token;
+      }
+    }
+
     return null;
   }
 
--- src/OAuth/TokenStorage/TokenStorageInterface.php
+++ src/OAuth/TokenStorage/TokenStorageInterface.php
@@ -1,6 +1,8 @@
 <?php
 namespace Fortifi\Api\Core\OAuth\TokenStorage;
 
+use Fortifi\Api\Core\OAuth\Tokens\IToken;
+
 interface TokenStorageInterface
 {
   /**
@@ -8,18 +10,19 @@ interface TokenStorageInterface
    *
    * @param string $key location key to store the token in
    *
-   * @param string $token
+   * @param IToken $token
    *
    * @return bool
    */
-  public function storeToken($key, $token);
+  public function storeToken($key, IToken $token);
 
   /**
    * Retrieve a token from storage
    *
-   * @param string $key location key for token
+   * @param string   $key      location key for token
+   * @param callable $retrieve method to retireve token
    *
-   * @return string|null
+   * @return IToken|null
    */
-  public function retrieveToken($key);
+  public function retrieveToken($key, callable $retrieve = null);
 }
