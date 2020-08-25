bbad71398bff314b2a0a68f1e151bde2b1a980d1
Jim Eagle <jimeagle@gmail.com>
2020-08-25 10:13:19 +0100

    Handle 409 Conflict headers (#3)
    * Handle conflict headers
    
    * Fix headers
    
    * Handle no headers
    
    * Fix
    
    * Fix
    
    * Dont wrap exceptions
    
    * Remove space
    
    * Use constants

--- src/Exceptions/Client/ConflictException.php
+++ src/Exceptions/Client/ConflictException.php
@@ -1,19 +1,30 @@
 <?php
 namespace Fortifi\Api\Core\Exceptions\Client;
 
+use Exception;
+
 class ConflictException extends ClientApiException
 {
+  const FIELD = 'x-fortifi-conflict-field';
+  const VALUE = 'x-fortifi-conflict-value';
+  const OWNER = 'x-fortifi-conflict-owner';
+
+  public $field;
+  public $value;
+  public $owner;
+
   /**
    * @inheritDoc
    */
-  public function __construct(
-    $message = '', $code = 409, \Exception $previous = null
-  )
+  public function __construct($message = '', $code = 409, Exception $previous = null)
   {
-    if(empty($message))
-    {
-      $message = 'Conflict';
-    }
-    parent::__construct($message, $code, $previous);
+    parent::__construct($message ?: 'Conflict', $code, $previous);
+  }
+
+  public function handleHeaders(array $headers)
+  {
+    $this->field = !empty($headers[static::FIELD]) ? $headers[static::FIELD][0] : null;
+    $this->value = !empty($headers[static::VALUE]) ? $headers[static::VALUE][0] : null;
+    $this->owner = !empty($headers[static::OWNER]) ? $headers[static::OWNER][0] : null;
   }
 }
